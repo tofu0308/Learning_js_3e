d680e4640c2a673721a8973962f7b366bd004c98
tofu0308 <isisaka2000@gmail.com>
2017-04-06 01:26:08 +0900

    2-6

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ console.log('load file.');
 
 $(document).ready(function(){
 	console.log('jQuery');
+
+	'use strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+	/* draw */
+	paper.view.draw;
+	let c = Shape.Circle(200, 200, 50);
+	c.fillColor  =  'green';
 });
