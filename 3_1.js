05315b5528f5c5426412b2c0ac7dbcc0010c474f
tofu0308 <isisaka2000@gmail.com>
2017-04-09 19:19:58 +0900

    3_6

--- 3_1.js
+++ 3_1.js
@@ -49,3 +49,29 @@ console.log(nan);
 console.log(1/0);
 console.log(-1/0);
 console.log(Infinity/Infinity);
+
+
+
+const small = Number.EPSILON;
+console.log(small);
+
+const bigInt = Number.MAX_SAFE_INTEGER;
+console.log(bigInt);
+
+const max = Number.MAX_VALUE;
+console.log(max);
+
+const minInt = Number.MIN_SAFE_INTEGER;
+console.log(minInt);
+
+const min = Number.MIN_VALUE;
+console.log(min);
+
+const nInf = Number.NEGATIVE_INFINITY;
+console.log(nInf);
+
+const NaN2 = Number.NaN;
+console.log(NaN2);
+
+const inf2 = Number.POSITIVE_INFINITY;
+console.log(inf2);
