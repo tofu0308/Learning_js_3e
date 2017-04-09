8a90b0db1fd3436848660533ac44e24ef725172e
tofu0308 <isisaka2000@gmail.com>
2017-04-09 18:46:28 +0900

    3_3

---
+++ 3_1.js
@@ -0,0 +1,11 @@
+let currentTempC = 16;
+console.log(currentTempC);
+
+currentTempC = 22.5;
+console.log(currentTempC);
+
+let 現在の温度 = 16;
+let 目標温度, 部屋1='会議室A', 部屋2 = 'ロビー';
+console.log(目標温度);
+console.log(部屋1);
+console.log(部屋2);
