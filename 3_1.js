55490d32bc11b1ce1e2c0857a61bfa02aa501476
tofu0308 <isisaka2000@gmail.com>
2017-04-09 18:52:18 +0900

    3_4

--- 3_1.js
+++ 3_1.js
@@ -9,3 +9,9 @@ let 目標温度, 部屋1='会議室A', 部屋2 = 'ロビー';
 console.log(目標温度);
 console.log(部屋1);
 console.log(部屋2);
+
+let room1 = '会議室A';
+console.log(room1);
+
+let currentRoom = room1;
+console.log(currentRoom);
