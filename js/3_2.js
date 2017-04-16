dc3b5c5638511f0f19b6501aec1a1b107c6e34e5
tofu0308 <isisaka2000@gmail.com>
2017-04-16 17:34:08 +0900

    3_7_5

--- js/3_2.js
+++ js/3_2.js
@@ -37,3 +37,10 @@ const mulutiLine2 = "line2_1\n" +
 
 console.log(mulutiLine);
 console.log(mulutiLine2);
+
+//3_7_5
+const result1 = 3 + '30';
+const result2 = 3 * '30';
+
+console.log(result1);
+console.log(result2);
