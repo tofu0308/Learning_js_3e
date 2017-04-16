2135ae19669e168bfdc2246377608de8a0e403fa
tofu0308 <isisaka2000@gmail.com>
2017-04-16 17:20:26 +0900

    3_7_2

--- js/3_2.js
+++ js/3_2.js
@@ -1,3 +1,4 @@
+//3_7_1
 const dialog1 = "dialog1 \"escape\"";
 const dialog2 = 'dialog2 \'escape\'';
 const dialog3 = `dialog3 "escape"`;
@@ -9,3 +10,14 @@ console.log(dialog1);
 console.log(dialog2);
 console.log(dialog3);
 console.log(d1 === d3);
+
+//3_7_2
+const newLine = `line1\nline2`;
+const bq = `バッククォートをエスケープ=>　\``
+
+const price = `900`;
+const dollor = `価格は${price}です`
+
+console.log(newLine);
+console.log(bq);
+console.log(dollor);
