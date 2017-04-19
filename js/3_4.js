74dbf6fadf9fd63fae3b9bae904a35b699952063
tofu0308 <isisaka2000@gmail.com>
2017-04-20 00:35:05 +0900

    3_16

--- js/3_4.js
+++ js/3_4.js
@@ -85,3 +85,8 @@ console.log(halloweenParty.getHours());
 console.log(halloweenParty.getMinutes());
 console.log(halloweenParty.getSeconds());
 console.log(halloweenParty.getMilliseconds());
+
+
+//3_16
+const email = /\b[a-z0-9]+@[a-z_-]+(?:\.[a-z]+)+b/;
+const phone = /(:?\+1)?(:?\(\d\{3}\)\s?)|\d\{3}[\s-]?d{4}/;
