5a5ffadf4b8d447fab924b27c67e8520347aa593
tofu0308 <isisaka2000@gmail.com>
2017-05-11 01:54:16 +0900

    5_13

--- js/5_1.js
+++ js/5_1.js
@@ -259,3 +259,18 @@ console.log(typeof "");
 console.log(typeof Symbol());//ES2015~
 console.log(typeof function(){});
 
+//5_13
+console.log("--------5_13-------");
+
+let lv, lv0;
+lv = lv0 = 9.8;
+console.log(lv);//9.8
+console.log(lv0);//9.8
+
+const nums = [3, 5, 15, 7, 5];
+let n, i = 0;
+while(i < nums.length,(n = nums[i++]) < 10){
+	console.log(`10より小さい数字を発見:${n}`);
+}
+console.log(`10より大きい数字を発見:${n}`);
+console.log(`未チェックの数字の個数:${nums.length - i}`);
