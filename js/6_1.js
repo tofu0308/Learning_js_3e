4056802ea39c98011d97e0d3eaf46a719cd58f55
tofu0308 <isisaka2000@gmail.com>
2017-05-13 15:21:01 +0900

    6_2

--- js/6_1.js
+++ js/6_1.js
@@ -8,5 +8,24 @@ function getGreeting(){
 const message = getGreeting();
 console.log(message);
 
+//6_2
+console.log("--------6_2-------");
 
+console.log(getGreeting());//関数を呼び出す
+console.log(getGreeting);//関数を参照する
 
+const f = getGreeting;
+console.log(f());
+
+const o = {};
+o.f = getGreeting;
+console.log(o.f());
+
+console.log("---------------");
+
+const arr = [1, 2, 3];
+arr[1] = getGreeting;
+console.log(arr);
+
+const message2 = arr[1]();
+console.log(message2);
