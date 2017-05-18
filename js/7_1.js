efb5514d17ca2fd86283cceef78b836a256d1e62
tofu0308 <isisaka2000@gmail.com>
2017-05-19 00:20:58 +0900

    7_2

--- js/7_1.js
+++ js/7_1.js
@@ -6,3 +6,29 @@ function  f(x){
 }
 console.log(f(5));
 //console.log(x); //error
+
+//7_2
+console.log("--------7_2-------");
+
+function f2_1(){
+	console.log("f2_1");
+}
+
+function f2_2(){
+	console.log("f2_2");
+}
+
+f2_2();
+f2_1();
+f2_2();
+
+console.log("---------------");
+
+const x2 = 3;
+function f2_2_1(){
+	console.log(x2);
+	//console.log(y2); //error:y2 is not defined
+}
+
+f2_2_1();
+const y2 =3;
