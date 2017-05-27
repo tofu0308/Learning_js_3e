10833cec8edf398c9678d408d94bf64754311b99
tofu0308 <isisaka2000@gmail.com>
2017-05-27 16:03:34 +0900

    8_5

--- js/8_1.js
+++ js/8_1.js
@@ -415,4 +415,57 @@ console.table(先頭文字で分類したもの);
 
 console.log("---------------");
 
+const data = [3.3, 5, 7.2, 12, 4, 6, 10.3];
+const stats = data.reduce((a, x) => {
+	a.N++;
+	let delta = x - a.mean;
+	a.mean += delta/a.N;
+	a.M2 = delta*(x - a.mean);
+	return a;
+}, {N: 0, mean: 0, M2: 0});
+
+if(stats.N > 2) {
+	stats.variance = stats.M2 / (stats.N -1);
+	stats.stdev = Math.sqrt(stats.variance);
+}
+console.log(stats);
+
+console.log("---------------");
+
+const words5_5 = [	"Beachball", "Rodeo", "Angel", "Aardvark", "Xlyophone", "November", "Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bali"];
+const longWords = words5_5.reduce((a, w) => w.length > 6 ?  a+" "+w : a , "").trim();
+console.log(longWords);
+
+const 単語リスト5_5 = ["ビーチボール", "ルービックキューブ", "ブダペスト", "トロッコ", "コンゴ", "ゴースト", "トーラス", "スマート", "トンガ", "ガラパゴス", "ストリート", "トーマス", "ストレッチ", "チベット", "トキ", "キツツキ", "キリン"];
+const 長い単語 = 単語リスト5_5.reduce((a, w) => w.length >= 5 ?  a+" "+w : a , "").trim();
+console.log(長い単語);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
