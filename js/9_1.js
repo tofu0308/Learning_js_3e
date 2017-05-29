65493c12de9d0e37c617fa8e5abdbbcbe289c4c8
tofu0308 <isisaka2000@gmail.com>
2017-05-29 17:52:27 +0900

    9_1

--- js/9_1.js
+++ js/9_1.js
@@ -1,3 +1,13 @@
 //9_1
 console.log("--------9_1-------");
 
+//9_1_1
+const SYM1_1 = Symbol();
+const o1_1 =  {a: 1, b: 2, c: 3, [SYM1_1]: 4};
+
+for(let prop in o1_1){
+	if(!o1_1.hasOwnProperty(prop)) continue;
+	console.log(`${prop}:${o1_1[prop]}`);
+}
+
+console.log("---------------");
