7177f837d942aba9afb05652f6ea585b73cd3305
tofu0308 <isisaka2000@gmail.com>
2017-05-29 18:43:05 +0900

    9_2_2

--- js/9_1.js
+++ js/9_1.js
@@ -84,3 +84,45 @@ console.log("---------------");
 	console.log(car1.userGear);
 	console.log(car2.userGear);
 }
+
+
+console.log("---------------");
+//9_2_2
+
+{
+	class Car {
+		constructor(make, model){
+	 		this.make = make;
+	 		this.model = model;
+	 		this._userGears = ['P', 'N', 'R', 'D'];
+	 		this._userGear = this._userGears[0];
+		}
+
+		get userGear() {return this._userGear;}
+		set userGear(value) {
+			if(this._userGears.indexOf(value) < 0) {
+				throw new Error(`ギア指定が正しくない: ${value}`)
+			}
+			this._userGear = value;
+		}
+
+		shift(gear) {this.userGear = gear;}
+
+	}
+
+	const car1 = new Car("Tesla", "Model S");
+	const car2 = new Car("Mazda", "3i");
+	console.log(car1);
+	console.log(car2);
+
+	car1.shift('D');
+	car2.shift('R');
+	console.log(car1.userGear);
+	console.log(car2.userGear);
+
+	//car1.userGear = "X"; //error : Uncaught Error: ギア指定が正しくない: X
+}
+
+
+
+
