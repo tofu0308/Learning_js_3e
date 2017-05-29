732e1ba8d3ce272c964a874520ebbe417649f870
tofu0308 <isisaka2000@gmail.com>
2017-05-29 18:30:50 +0900

    9_2_1

--- js/9_1.js
+++ js/9_1.js
@@ -40,3 +40,47 @@ console.log("---------------");
 
 const o1_2_2 = {apple:1, xochitl:2, balloon:3, guitar:4, xylophone:5, };
 Object.keys(o1_2_2).filter(prop => prop.match(/^x/)).forEach(prop => console.log(`${prop}: ${o1_2_2[prop]}`));
+
+//9_2
+console.log("--------9_2-------");
+
+//9_2_1
+class Car {
+	constructor() {
+	}
+}
+
+const car1 = new Car();
+const car2 = new Car();
+
+console.log(car1 instanceof Car)
+console.log(car2 instanceof Car)
+console.log(car1 instanceof Array);
+
+console.log("---------------");
+{
+	class Car {
+		constructor(make, model){
+	 		this.make = make;
+	 		this.model = model;
+	 		this.userGears = ['P', 'N', 'R', 'D'];
+	 		this.userGear = this.userGears[0];
+		}
+		shift(gear){
+			if(this.userGears.indexOf(gear) < 0) {
+				throw new Error('ギア指定が正しくない: ${gear}');
+			}
+			this.userGear = gear;
+		}
+	}
+
+	const car1 = new Car("Tesla", "Model S");
+	const car2 = new Car("Mazda", "3i");
+	console.log(car1);
+	console.log(car2);
+
+	car1.shift('D');
+	car2.shift('R');
+	console.log(car1.userGear);
+	console.log(car2.userGear);
+}
