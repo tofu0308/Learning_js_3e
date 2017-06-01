0b8905e03fedb97251c82ae9f8427fd2fd5478b4
tofu0308 <isisaka2000@gmail.com>
2017-06-02 01:11:15 +0900

    9_2_8

--- js/9_1.js
+++ js/9_1.js
@@ -331,3 +331,32 @@ console.log("---------------");
 	console.log(m instanceof Vehicle);
 }
 
+console.log("---------------");
+//9_2_8
+
+{
+	class Super {
+		constructor() {
+			this.name = 'Super';
+			this.isSuper = true;
+		}
+	}
+
+	Super.prototype.sneaky = '非推奨！';
+
+	class Sub extends Super {
+		constructor() {
+			super();
+			this.name = 'Sub';
+			this.isSub = true;
+		}
+	}
+
+	const obj = new Sub();
+
+	for(let p in obj) {
+		console.log(`${p}: ${obj[p]}` + (obj.hasOwnProperty(p) ? '' : '(継承)'));
+	}
+
+
+}
