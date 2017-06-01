141711af648e2f5255699a36990240ffde761bb0
tofu0308 <isisaka2000@gmail.com>
2017-06-02 01:42:24 +0900

    9_3

--- js/9_1.js
+++ js/9_1.js
@@ -357,6 +357,110 @@ console.log("---------------");
 	for(let p in obj) {
 		console.log(`${p}: ${obj[p]}` + (obj.hasOwnProperty(p) ? '' : '(継承)'));
 	}
+}
+
+//9_3
+console.log("--------9_2-------");
+
+{
+	class Car {
+		cnstructor(){}
+	}
+
+	class InsurancePolicy {
+		//保険契約
+	}
+
+	function makeInsurable(o) {
+		o.addInsurancePolicy = function(p) {this.insurancePolicy = p;}
+		o.getInsurancePolicy = function() {return this.insurancePolicy;}
+		o.isInsured = function() {return !!this.insurancePolicy;}
+	}
+
+	/* 下記はエラー
+	makeInsurable(Car);
+	const car1 = new Car;
+	car1.addInsurancePolicy(new InsurancePolicy());
+	*/
+
+	const car1 = new Car();
+	makeInsurable(car1);
+	console.log(car1.isInsured());
+
+	car1.addInsurancePolicy(new InsurancePolicy());
+	console.log(car1.isInsured());
+
+}
+
+console.log("---------------");
+
+{
+	class Car {
+		cnstructor(){}
+	}
+
+	class InsurancePolicy {
+		//保険契約
+	}
+
+	function makeInsurable(o) {
+		o.addInsurancePolicy = function(p) {this.insurancePolicy = p;}
+		o.getInsurancePolicy = function() {return this.insurancePolicy;}
+		o.isInsured = function() {return !!this.insurancePolicy;}
+	}
+
+	makeInsurable(Car.prototype);
 
 
+	const car1 = new Car();
+	console.log(car1.isInsured());
+	car1.addInsurancePolicy(new InsurancePolicy());
+	console.log(car1.isInsured());
+
+	console.log("---");
+	
+	const car2 = new Car();
+	console.log(car2.isInsured());
+	car2.addInsurancePolicy(new InsurancePolicy());
+	console.log(car2.isInsured());
+
+}
+
+console.log("---------------");
+
+{
+	class Car {
+		cnstructor(){}
+	}
+
+	class InsurancePolicy {
+		//保険契約
+	}
+
+	const ADD_POLICY = Symbol();
+	const GET_POLICY = Symbol();
+	const IS_INSURED = Symbol();
+	const _POLICY = Symbol();
+
+
+	function makeInsurable(o) {
+		o[ADD_POLICY] = function(p) {this[_POLICY] = p;}
+		o[GET_POLICY] = function() {return thisthis[_POLICY];}
+		o[IS_INSURED] = function() {return !!this[_POLICY];}
+	}
+
+	makeInsurable(Car.prototype);
+
+
+	const car1 = new Car();
+	console.log(car1[IS_INSURED]());
+	car1[ADD_POLICY](new InsurancePolicy());
+	console.log(car1[IS_INSURED]());
+
+	console.log("---");
+	
+	const car2 = new Car();
+	console.log(car2[IS_INSURED]());
+	car2[ADD_POLICY](new InsurancePolicy());
+	console.log(car2[IS_INSURED]());
 }
