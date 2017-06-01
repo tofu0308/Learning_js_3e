7642330f20c73ab99ee671aed68f849c0e32ee6a
tofu0308 <isisaka2000@gmail.com>
2017-06-02 00:31:58 +0900

    9_2_5

--- js/9_1.js
+++ js/9_1.js
@@ -241,8 +241,41 @@ console.log("---------------");
 	console.log(car1.userGear);
 
 	//car2.shift('d'); //error
-
-
 }
 
+console.log("---------------");
+//9_2_5
+
+{
+	class Car {
+		static getNextVin() {
+			return Car.nextVin++;
+		}
+		constructor(make, model) {
+			this.make = make;
+			this.model = model;
+			this.vin = Car.getNextVin();
+		}
+		static areSimilar(car1, car2) {
+			return car1.make === car2.make && car1.model === car2.model;
+		}
+		static areSame(car1, car2) {
+			return car1.vin === car2.vin;
+		}
+	}
+	Car.nextVin = 0;
+
+	const car1 = new Car("Tesla", "Model S");
+	const car2 = new Car("Mazda", "3i");
+	const car3 = new Car("Mazda", "3i");
+
+	console.log(car1.vin);
+	console.log(car2.vin);
+	console.log(car3.vin);
+
+	console.log(Car.areSimilar(car1, car2));
+	console.log(Car.areSimilar(car2, car3));
+	console.log(Car.areSame(car2, car3));
+	console.log(Car.areSame(car2, car2));
+}
 
