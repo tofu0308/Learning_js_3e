1fd12c04bebf74eae487e09ad288491585e8d773
tofu0308 <isisaka2000@gmail.com>
2017-06-04 17:02:32 +0900

    11_2

--- js/11_1.js
+++ js/11_1.js
@@ -15,3 +15,23 @@ console.log("--------11_1-------");
 		console.log(`正しい形式のEmailアドレス：${validatedEmail}`)
 	}
 }
+
+//11_2
+console.log("--------11_2-------");
+
+{
+	const email = null;
+	//const email = "hoge@test.com";
+
+	try {
+		const validatedEmail = validateEmail(email);
+		console.log(validatedEmail);
+		if(validatedEmail instanceof Error){
+			console.error(validatedEmail.message);
+		}else {
+			console.log(`正しい形式のEmailアドレス：${validatedEmail}`)
+		}
+	}catch(err){
+		console.log(`無効なEmailアドレスです：${err.message}`);
+	}
+}
