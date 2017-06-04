eabb2fd021f870da29f76e776dc98863018b9f2e
tofu0308 <isisaka2000@gmail.com>
2017-06-04 17:19:54 +0900

    11_4

--- js/11_1.js
+++ js/11_1.js
@@ -35,3 +35,50 @@ console.log("--------11_2-------");
 		console.log(`無効なEmailアドレスです：${err.message}`);
 	}
 }
+
+//11_4
+console.log("--------11_4-------");
+
+
+{
+	function a(){
+		console.log(`a:bを呼び出す前`);
+		b();
+		console.log(`a:終了`);
+	}
+
+	function b(){
+		console.log(`b:cを呼び出す前`);
+		c();
+		console.log(`b:終了`);
+	}
+
+	function c(){
+		console.log(`c:エラーをスローする`);
+		throw new Error(`c error`);
+		console.log(`:終了`);
+	}
+
+	function d(){
+		console.log(`d:cを呼び出す前`);
+		c();
+		console.log(`d終了`);
+	}
+
+	try {
+		a();
+	}catch(err) {
+		console.log(`---a開始前のerr.stack---`);
+		console.log(err.stack);
+		console.log(`---終わり---`);
+	}
+
+	try {
+		d();
+	}catch(err){
+		console.log(`---d開始前のerr.stack---`);
+		console.log(err.stack);
+		console.log(`---終わり---`);		
+	}
+}
+
