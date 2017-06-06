1da795aa34cd7326c21e06b22d694b9d7ec73070
tofu0308 <isisaka2000@gmail.com>
2017-06-07 00:56:35 +0900

    12_1_2

--- js/12_1.js
+++ js/12_1.js
@@ -156,43 +156,25 @@ console.log("---------------");
 	},10*1000)
 }
 
+console.log("---------------");
 
+{
+	const book = [
+		"あるところに初老の夫婦が住んでたとさ",
+		"夫は山へ妻は川へ行ったとさ。",
+		"妻が川で洗濯をしていると、",
+		"上流から桃が流れてきたとさ。",
+		"ドンブラコッコ、スッコッコ",
+		"ドンブラコッコ、スッコッコ",
+	];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	const it = book[Symbol.iterator]();
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+}
 
