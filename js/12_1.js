a013e53e63a7e2e257935ce5c0946c5153e5190f
tofu0308 <isisaka2000@gmail.com>
2017-06-09 01:52:14 +0900

    12_1_3

--- js/12_1.js
+++ js/12_1.js
@@ -178,3 +178,71 @@ console.log("---------------");
 	console.log(it.next());
 }
 
+console.log("---------------");
+//12_1_3
+
+{
+	class FibonacciSequence {
+		[Symbol.iterator](){
+			let a = 0, b = 1;
+			return {
+				next(){
+					let rval = {value: b, done: false};
+					b += a;
+					a = rval.value;
+					return rval;
+				}
+			}
+		}
+	}
+
+	const fib = new FibonacciSequence();
+	let i = 0;
+	for(let n of fib){
+		console.log(`${i+1}: ${n}`);
+		if(++i > 99) {
+			break;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
