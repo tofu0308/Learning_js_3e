8981b0e122ee11c11faaca0982fd1c325f60b0d1
tofu0308 <isisaka2000@gmail.com>
2017-06-09 01:57:11 +0900

    12_2

--- js/12_1.js
+++ js/12_1.js
@@ -206,10 +206,37 @@ console.log("---------------");
 	}
 }
 
+//12_2
+console.log("--------12_2-------");
 
+{
+	function* rainbow(){
+		yield '赤';
+		yield '青';
+		yield '黄';
+		yield '緑';
+		yield '青';
+		yield '水色';
+		yield '紫';
+	}	
 
+	const it = rainbow();
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
+	console.log(it.next());
 
+	for(let color of rainbow()){
+		console.log(color);
+	}
+}
 
+console.log("---------------");
 
 
 
