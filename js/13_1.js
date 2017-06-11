309d33519c2b6521c96d91fa4ea0fc372f81ad9e
tofu0308 <isisaka2000@gmail.com>
2017-06-11 17:39:10 +0900

    13_1

--- js/13_1.js
+++ js/13_1.js
@@ -1,2 +1,30 @@
 //13_1
 console.log("--------13_1-------");
+
+{
+	const year = new Date().getFullYear();
+	if(year % 4 !==0){
+		console.log(`${year}年は閏年ではない`);
+	}else if(year % 100 !==0){
+		console.log(`${year}年は閏年である`);
+	}else if(year % 400 !==0){
+		console.log(`${year}年は閏年ではない`);
+	}
+}
+
+console.log("---------------");
+
+{
+	function printLeapYearStatus() {
+		const year = new Date().getFullYear();
+		if(year % 4 !==0){
+			console.log(`${year}年は閏年ではない`);
+		}else if(year % 100 !==0){
+			console.log(`${year}年は閏年である`);
+		}else if(year % 400 !==0){
+			console.log(`${year}年は閏年ではない`);
+		}		
+	}
+
+	printLeapYearStatus();
+}
