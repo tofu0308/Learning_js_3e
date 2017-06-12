46b3fe0f0e8a97dbedac103e09a49c2c319f66d7
tofu0308 <isisaka2000@gmail.com>
2017-06-13 00:51:26 +0900

    13_5

--- js/13_1.js
+++ js/13_1.js
@@ -212,7 +212,18 @@ console.log("---");
 
 console.log("---");
 
-
+{
+	//カウントダウン
+	function loopBody(i){
+		setTimeout(function(){
+			console.log(i === 0 ? "go" : i);
+		} ,(5 - i) * 1000);
+	}
+	var i;
+	for(i=5; i>=0; i--){
+		loopBody(i);
+	}
+}
 
 
 
