1c1325afd313c3d53f00d04a6fd32d5f7d56bb0b
tofu0308 <isisaka2000@gmail.com>
2017-06-13 00:56:46 +0900

    13_5

--- js/13_1.js
+++ js/13_1.js
@@ -225,7 +225,18 @@ console.log("---");
 	}
 }
 
+console.log("---");
 
+{
+	var i;
+	for(i=5; i>=0; i--){
+		(function(i){
+			setTimeout(function(){
+				console.log(i === 0 ? "go" : i);
+			},(5 -i) * 1000);
+		})(i);
+	}
+}
 
 
 
