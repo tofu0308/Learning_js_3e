1b55e37433960ac702ee67ae4767e2f176079d83
tofu0308 <isisaka2000@gmail.com>
2017-06-14 00:22:58 +0900

    13_6_1

--- js/13_1.js
+++ js/13_1.js
@@ -275,8 +275,50 @@ console.log("--------13_6-------");
 	console.log(answer2);
 }
 
+console.log("---------------");
+//13_6_1
+
+{
+	const sin = Math.sin;
+	const cos = Math.cos;
+	const theta = Math.PI/4;
+	const zoom = 2;
+	const offset = [1, -3];
+
+	const pipeline = [
+		function rotate(p) {
+			return {
+				x: p.x * cos(theta) - p.y * sin(theta),
+				y: p.x * sin(theta) + p.y * cos(theta),
+			};
+		},//pipline[0]
+
+		function scale(p){
+			return {
+				x: p.x * zoom,
+				y: p.y * zoom
+			}
 
 
+		},//pipeline[1]
+
+		function translate(p) {
+			return {
+				x: p.x + offset[0],
+				y: p.y + offset[1]
+			};
+		},//pipeline[2]
+	];
+
+	const p = {x:1, y:1}
+	console.log(p);
+
+	let p2 = p;
+	for(let i=0; i < pipeline.length; i++){
+		p2 = pipeline[i](p2);
+		console.log(p2);
+	}
+}
 
 
 
