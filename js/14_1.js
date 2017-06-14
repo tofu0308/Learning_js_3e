725e37f1887795f2a20dec007e4a9045f1b59cdb
tofu0308 <isisaka2000@gmail.com>
2017-06-14 22:55:36 +0900

    14_2

--- js/14_1.js
+++ js/14_1.js
@@ -1,2 +1,15 @@
 //14_2
 console.log("--------14_2-------");
+
+{
+	console.log("setTimeoutの前" + new Date());
+	function f(){
+		console.log("関数fの中" + new Date());
+	}
+
+	//10秒後にfを実行
+	setTimeout(f, 10*1000);
+
+	console.log("setTimeoutの後");
+	console.log("こちらもsetTimeoutの後");
+}
