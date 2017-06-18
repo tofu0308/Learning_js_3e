ca317a96b4253cd7041f077318627794be2173a3
tofu0308 <isisaka2000@gmail.com>
2017-06-18 16:03:52 +0900

    14_3_4

--- js/14_4.js
+++ js/14_4.js
@@ -42,7 +42,7 @@ console.log("--------14_3-------");
 		console.log("カウントダウン失敗(p):" + err.message);
 	});
 }
-*/
+
 console.log("---------------");
 
 {
@@ -77,5 +77,93 @@ console.log("---------------");
 		}
 
 	);
-
 }
+*/
+
+{
+	function countdown(seconds){
+		return new Promise(
+			function(onFulfilled, onRejected){
+				const timeoutIds = [];
+				for(let i = seconds; i>=0; i--){
+					timeoutIds.push(setTimeout(
+						function(){
+							if(i===13){
+								timeoutIds.forEach(clearTimeout);
+								onRejected(new Error(`不吉な数字${i}エラー`));
+							}else if(i>0){
+								console.log(`${i}...`);
+							}else{
+								console.log("GO!");
+								onFulfilled();
+							}
+						},(seconds-i)*1000));
+				}
+			}
+		);
+	}
+
+
+	function launch(){
+		return new Promise(function(onFulfilled, onRejected){
+			console.log("発射");
+			setTimeout(function(){
+				onFulfilled("周回軌道に乗った");
+			}, 2*1000);
+
+		});
+	}
+
+	countdown(12)
+		.then(launch)
+		.then(function(msg){
+			console.log(msg);
+		})
+		.catch(function(err){
+			console.error("トラブル発生:" +err );
+
+		});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
