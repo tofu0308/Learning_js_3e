dc5c3afa373ea84dccda562a310f010bb829678c
tofu0308 <isisaka2000@gmail.com>
2017-09-21 00:01:47 +0900

    15_2

--- js/15_1.js
+++ js/15_1.js
@@ -5,3 +5,63 @@ console.log("--------15_1-------");
 	console.log(d);
 	console.log(d.valueOf());
 }
+
+
+//15_2
+console.log("--------15_2-------");
+{
+	let d = new Date();
+
+	console.log(`現在時刻 => ${d}`);
+
+	d = new Date(2017, 0);
+	console.log(`指定年月を取得（月は０から始まる） => ${d}`);
+
+	d = new Date(2017, 1);
+	console.log(`指定年月を取得（月は０から始まる） => ${d}`);
+
+	d = new Date(2017, 1, 14);
+	console.log(`指定年月日を取得 => ${d}`);
+
+	d = new Date(2017, 1, 14, 13);
+	console.log(`指定年月日,時刻を取得 => ${d}`);
+
+	d = new Date(2017, 1, 14, 13, 25);
+	console.log(`指定年月日,時刻(分まで)を取得 => ${d}`);
+
+	d = new Date(2017, 1, 14, 13, 25, 10);
+	console.log(`指定年月日,時刻(分,秒まで)を取得 => ${d}`);
+
+	d = new Date(2017, 1, 14, 13, 25, 45, 800);
+	console.log(`指定年月日,時刻(分、秒、ミリ秒まで)を取得 => ${d}`);
+
+	console.log(`-------`);
+
+	d  = new Date(0);
+	console.log(`UNIXエポックからの経過時刻（0ms経過）=> ${d}`);
+
+	d  = new Date(1000);
+	console.log(`UNIXエポックからの経過時刻（1s経過）=> ${d}`);
+
+	d  = new Date(1463443200000);
+	console.log(`UNIXエポックからの経過時刻 => ${d}`);
+
+	d  = new Date(-365 * 24 * 60 * 60 * 1000);
+	console.log(`UNIXエポックからの経過時刻(マイナス) => ${d}`);
+
+	console.log(`-------`);
+
+	d  = new Date('June 14, 1903');
+	console.log(`文字列を指定 => ${d}`);
+
+	d  = new Date('June 14, 1902 GMT-0000');
+	console.log(`文字列を指定 => ${d}`);
+
+	/*
+	日本語表記は不可
+	d  = new Date('2020年7月10日');
+	console.log(`文字列を指定 => ${d}`);
+	*/
+
+}
+
