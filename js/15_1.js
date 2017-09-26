1f442a697787f7748e3c6089777bef32a88c48e7
tofu0308 <isisaka2000@gmail.com>
2017-09-27 00:04:40 +0900

    15_6

--- js/15_1.js
+++ js/15_1.js
@@ -90,8 +90,22 @@ console.log("--------15_5-------");
 	after.d = new Date(after.d);
 	console.log(after.d instanceof Date);
 	console.log(after.d);
-
-
-
 }
 
+	console.log(`-------`);
+
+{
+	const before = { d: new Date().valueOf() };
+	console.log(before.d);
+	console.log(typeof before.d);
+
+	const json = JSON.stringify(before);
+	console.log(json);
+
+	const after = JSON.parse(json);
+	console.log(after);
+	console.log(typeof after.d);
+
+	const d = new Date(after.d);
+	console.log(d);
+}
