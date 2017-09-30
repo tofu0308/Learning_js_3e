389af8d5e91574961bcdf8a3dda0e39e0a729409
tofu0308 <isisaka2000@gmail.com>
2017-09-30 16:19:28 +0900

    16_3_2

--- js/16_1.js
+++ js/16_1.js
@@ -104,6 +104,22 @@ console.log("--------16_3-------");
 	console.log(Math.hypot(2, 3, 4));
 }
 
+console.log(`-------`);
+
+//16_3_2
+{
+	console.log(Math.log(Math.E));
+	console.log(Math.log(17.5));
+
+	console.log(Math.log10(10));
+	console.log(Math.log10(16.7));
+
+	console.log(Math.log2(2));
+	console.log(Math.log2(5));
+
+	console.log(Math.log1p(Math.E - 1));
+	console.log(Math.log1p(17.5));
+}
 
 
 
