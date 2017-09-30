3d07364a3bc781f6e6e64a041e9d38571894bef5
tofu0308 <isisaka2000@gmail.com>
2017-09-30 14:38:19 +0900

    16_1_1

---
+++ js/16_1.js
@@ -0,0 +1,14 @@
+//16_1
+console.log("--------16_1-------");
+{
+	const x = 19.51;
+
+	//少数点以下の桁数を指定
+	console.log(x.toFixed(3));
+	console.log(x.toFixed(2));
+	console.log(x.toFixed(1));
+	console.log(x.toFixed(0));
+}
+
+	console.log(`-------`);
+
