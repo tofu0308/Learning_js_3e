2c7a3a63e8f0e1d5e03b995f3c19caf955da4b69
tofu0308 <isisaka2000@gmail.com>
2017-09-30 17:59:22 +0900

    16_5

--- js/16_1.js
+++ js/16_1.js
@@ -211,9 +211,22 @@ console.log("--------16_4-------");
 //16_5
 console.log("--------16_5-------");
 
+{
+	console.log(Math.sinh(0));
+	console.log(Math.sinh(1));
+	
+	console.log(Math.cosh(0));
+	console.log(Math.cosh(1));
 
+	console.log(Math.tanh(0));
+	console.log(Math.tanh(1));
 
+	console.log(Math.asinh(0));
+	console.log(Math.asinh(1));
 
+	console.log(Math.acosh(0));
+	console.log(Math.acosh(1));
 
-
-
+	console.log(Math.atanh(0));
+	console.log(Math.atanh(1));
+}
