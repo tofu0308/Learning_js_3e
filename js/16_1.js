5587490353601916aa2625c2e49fde8684aa6101
tofu0308 <isisaka2000@gmail.com>
2017-09-30 15:58:24 +0900

    16_3_1

--- js/16_1.js
+++ js/16_1.js
@@ -79,3 +79,38 @@ console.log("--------16_2-------");
 	console.log(Math.SQRT1_2);
 	console.log(Math.SQRT2);
 }
+
+//16_3
+console.log("--------16_3-------");
+
+//16_3_1
+{
+	console.log(Math.pow(2, 10));
+	console.log(Math.pow(1.7, 2.3));
+
+	console.log(Math.sqrt(16));
+	console.log(Math.sqrt(15.5));
+
+	console.log(Math.cbrt(27));
+	console.log(Math.cbrt(22));
+
+	console.log(Math.exp(1));
+	console.log(Math.exp(5.5));
+
+	console.log(Math.expm1(1));
+	console.log(Math.expm1(5.5));
+
+	console.log(Math.hypot(3, 4));
+	console.log(Math.hypot(2, 3, 4));
+}
+
+
+
+
+
+
+
+
+
+
+
