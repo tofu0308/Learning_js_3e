9fb06ed0f808dadb73416d75fe18f736a86a3100
tofu0308 <isisaka2000@gmail.com>
2017-09-30 15:45:02 +0900

    16_2

--- js/16_1.js
+++ js/16_1.js
@@ -63,3 +63,19 @@ console.log(`-------`);
 	console.log(x.toString(2));
 }
 
+//16_2
+console.log("--------16_2-------");
+
+//定数
+{
+	console.log(Math.E);
+	console.log(Math.PI);
+
+	console.log(Math.LN2);
+	console.log(Math.LN10);
+	console.log(Math.LOG2E);
+	console.log(Math.LOG10E);
+
+	console.log(Math.SQRT1_2);
+	console.log(Math.SQRT2);
+}
