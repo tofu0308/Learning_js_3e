d2bb5a9dbc64bc20828896c697cdfd9ae3974cf5
tofu0308 <isisaka2000@gmail.com>
2017-09-30 17:53:59 +0900

    16_4

--- js/16_1.js
+++ js/16_1.js
@@ -169,6 +169,47 @@ console.log(`-------`);
 	console.log(1  + Math.floor((100 - +1) * Math.random()));
 }
 
+//16_4
+console.log("--------16_4-------");
+
+{
+	console.log(Math.sin(Math.PI/2));
+	console.log(Math.sin(Math.PI/4));
+
+	console.log(Math.cos(Math.PI));
+	console.log(Math.cos(Math.PI/4));
+
+	console.log(Math.tan(Math.PI/4));
+	console.log(Math.tan(0));
+
+	console.log(Math.asin(0));
+	console.log(Math.asin(Math.SQRT1_2));
+
+	console.log(Math.acos(0));
+	console.log(Math.acos(Math.SQRT1_2));
+
+	console.log(Math.atan(0));
+	console.log(Math.atan(Math.SQRT1_2));
+
+	console.log(Math.atan2(0, 1));
+	console.log(Math.atan2(1, 1));
+
+	console.log(`-------`);
+
+	const deg2rad　= (d) => {
+		return d/180*Math.PI;
+	}
+
+	const deg2deg　= (r) => {
+		return r/Math.PI*180;
+	}
+
+	console.log(deg2rad(60));
+	console.log(deg2deg(60));
+}
+
+//16_5
+console.log("--------16_5-------");
 
 
 
