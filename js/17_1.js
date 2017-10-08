6ea1c0d09358496d79599ca11dc573fffa109743
tofu0308 <isisaka2000@gmail.com>
2017-10-08 13:43:20 +0900

    17_8

--- js/17_1.js
+++ js/17_1.js
@@ -107,7 +107,6 @@ console.log("--------17_4-------");
 
 }
 
-
 //17_6
 console.log("--------17_6-------");
 {
@@ -124,3 +123,20 @@ console.log(`-------`);
 	console.log(matches);
 }
 
+
+//17_8
+console.log("--------17_8-------");
+{
+	const beer99 = "99 bottles of beer on the wall " + " take 1 down ans pass it around --" + "98 bottels of beer on the wall.";
+	const m1 = beer99.match(/0|1|2|3|4|5|6|7|8|9/g);
+	console.log(m1);
+
+	const m2 = beer99.match(/[0-9]/g);
+	console.log(m2);
+
+	const m3 = beer99.match(/[0-9a-z\-.]/ig);
+	console.log(m3);
+
+
+
+}
