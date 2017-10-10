1c73a5ce66d1cd8c03774d78792b27d1936a6616
tofu0308 <isisaka2000@gmail.com>
2017-10-10 23:04:25 +0900

    17_14

--- js/17_1.js
+++ js/17_1.js
@@ -270,6 +270,22 @@ console.log("--------17_13-------");
 		console.log(output2);
 }
 
+//17_14
+console.log("--------17_14-------");
+
+{
+	const html = 
+		`<img alt='A "simple" example'>` +
+		`<img alt="Don't abuse it!">`;
+	const imageTags = html.match(/<img alt=(['"']).*?\1>/g);
+
+	for(let i=0; imageTags && i<imageTags.length; i++){
+		console.log(imageTags[i]);
+	}
+}
+
+
+
 
 
 
