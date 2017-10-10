b7aa0cd82bb80bb797be0c59d1dc392bf1d3dd69
tofu0308 <isisaka2000@gmail.com>
2017-10-10 22:57:58 +0900

    17_13

--- js/17_1.js
+++ js/17_1.js
@@ -255,9 +255,20 @@ console.log("--------17_12-------");
 	console.log(matches);
 }
 
+//17_13
+console.log("--------17_13-------");
 
+{
+	const input = 
+		"Regex pros know the difference between\n" +
+		"<i>greedy</i> and <i>lazy</i> matching.";
 
+		const output = input.replace(/<i>(.*)<\/i>/ig, '<strong>$1</strong>');
+		console.log(output);
 
+		const output2 = input.replace(/<i>(.*?)<\/i>/ig, '<strong>$1</strong>');
+		console.log(output2);
+}
 
 
 
