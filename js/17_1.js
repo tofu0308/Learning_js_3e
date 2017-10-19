b4ba8bef4ecb3c491a81421f8c621f555b7bb9b8
tofu0308 <isisaka2000@gmail.com>
2017-10-20 00:53:41 +0900

    17_15

--- js/17_1.js
+++ js/17_1.js
@@ -323,7 +323,19 @@ console.log(`-------`);
 	console.log(r);
 }
 
+console.log(`-------`);
 
+{
+	const input = `One two three`;
+	let r = input.replace(/two/, '($`)');
+	console.log(r);
+
+	r = input.replace(/\w+/g, '($&)');
+	console.log(r);
+
+	r = input.replace(/two/, '($$)');
+	console.log(r);
+}
 
 
 
