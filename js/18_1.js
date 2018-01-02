2f63e579212265e3214c6e9fb8ff4925986b4138
tofu0308 <isisaka2000@gmail.com>
2018-01-03 00:42:09 +0900

    18_7 ~18_8

--- js/18_1.js
+++ js/18_1.js
@@ -109,7 +109,8 @@ console.log("--------18_7-------");
 			if(!containing.test(p.textContent)) continue;
 			p.classList.add('highligt18_7');
 		}
-	}	
+	}
+	higloghtParts('ユニーク')
 }
 
 {
@@ -120,6 +121,7 @@ console.log("--------18_7-------");
 			p.classList.remove('highligt18_7');
 		}
 	}
+	
 }
 
 
