58b786a0ace1f686c2a841d195ff59b3235aa675
tofu0308 <isisaka2000@gmail.com>
2018-01-13 16:17:46 +0900

    20_2

---
+++ js/20/app.js
@@ -0,0 +1,5 @@
+const amanda_caluclate = require('./amanda.js');
+const tyler_caluclate = require('./tyler.js');
+
+console.log(amanda_caluclate(1, 2, 5));
+console.log(tyler_caluclate(2));
---
+++ js/20/tyler.js
@@ -0,0 +1,5 @@
+function caluculate(r){
+	return 4/3 * Math.PI * Math.pow(r, 3);
+}
+
+module.exports = caluculate;
